4a27401f16584db1b99d6674ab9978b4f20f1dd5
Ryutaro YOSHIBA <ryuzee@gmail.com>
2017-11-30 14:04:36 +0900

    discard session

--- app/controllers/signup_cancel_controller.rb
+++ app/controllers/signup_cancel_controller.rb
@@ -1,6 +1,7 @@
 class SignupCancelController < ApplicationController
   def index
-    session["devise.user_attributes"] = nil
+    # session["devise.user_attributes"] = nil
+    reset_session
     redirect_to root_url
   end
 end
